f39f6c78e4496d57b7058ec4d6fe4cdaf01e124d
Sh3p <chritsfootballs@gmail.com>
2022-12-01 22:47:44 -0500

    fixes to town

--- Assets/Scripts/Enemy/EnemyWeaponSlotManager.cs
+++ Assets/Scripts/Enemy/EnemyWeaponSlotManager.cs
@@ -87,6 +87,6 @@ public class EnemyWeaponSlotManager : MonoBehaviour
 
    public void CloseDamageCollider(){
 
-    rightHandDamageCollider.EnableDamageCollider();
+    rightHandDamageCollider.DisableDamageCollider();
    }
 }
