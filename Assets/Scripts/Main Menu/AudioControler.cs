83c600e756b0c61cb182e8257ff4a5e54124dec7
Yah'hymbey Baruti Ali-Bey <yahhymbey.barutialibey@yahoo.com>
2022-12-03 22:50:46 -0500

    Audio Mixer Stuff

---
+++ Assets/Scripts/Main Menu/AudioControler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioControler : MonoBehaviour
+{
+
+    public AudioMixer theMixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("MasterVol")) {
+            theMixer.SetFloat("MasterVol", PlayerPrefs.GetFloat("MasterVol"));
+        }
+
+        if (PlayerPrefs.HasKey("MusicVol"))
+        {
+            theMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
+        }
+
+        if (PlayerPrefs.HasKey("SFXVol"))
+        {
+            theMixer.SetFloat("SFXVol", PlayerPrefs.GetFloat("SFXVol"));
+        }
+    }
+
+}
--- Assets/Scripts/Main Menu/OptionsMenu.cs
+++ Assets/Scripts/Main Menu/OptionsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using TMPro;
 
 public class OptionsMenu : MonoBehaviour
@@ -11,7 +12,12 @@ public class OptionsMenu : MonoBehaviour
     public List<ResItem> resolutions = new List<ResItem>();
     private int selectedResolution;
 
-    public TMP_Text resolutionLabel;
+    public TMP_Text resolutionLabel, masterLabel, musicLabel, sfxLabel;
+
+    public AudioMixer theMixer;
+
+    public Slider masterSlider, musicSlider, sfxSlider;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +54,14 @@ public class OptionsMenu : MonoBehaviour
             selectedResolution = resolutions.Count - 1;
             UpdateResLabel();
         }
+
+        float vol = 0f;
+        theMixer.GetFloat("MasterVol", out vol);
+        masterSlider.value = vol;
+        theMixer.GetFloat("MusicVol", out vol);
+        musicSlider.value = vol;
+        theMixer.GetFloat("SFXVol", out vol);
+        sfxSlider.value = vol;
     }
 
     public void ApplyGraphics() {
@@ -89,6 +103,27 @@ public class OptionsMenu : MonoBehaviour
         resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + "x" + resolutions[selectedResolution].vertical.ToString();
     }
 
+
+
+    public void SetMasterVolume() {
+        theMixer.SetFloat("MasterVol", masterSlider.value);
+
+        PlayerPrefs.SetFloat("MasterVol", masterSlider.value);
+    }
+
+    public void SetMusicVolume()
+    {
+        theMixer.SetFloat("MusicVol", musicSlider.value);
+
+        PlayerPrefs.SetFloat("MusicVol", musicSlider.value);
+    }
+
+    public void SetSFXVolume()
+    {
+        theMixer.SetFloat("SFXVol", sfxSlider.value);
+
+        PlayerPrefs.SetFloat("SFXVol", sfxSlider.value);
+    }
 
 }
 
